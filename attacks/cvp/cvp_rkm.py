d4903baabf1316d9aaae61f75a710d00e3daeba8
Le Tri Duc <159978981+ken3k06@users.noreply.github.com>
2025-12-09 21:27:12 +0700

    Update cvp_rkm.py

--- attacks/cvp/cvp_rkm.py
+++ attacks/cvp/cvp_rkm.py
@@ -43,6 +43,17 @@ def flatter(M):
         ARGS = []
 
     return parse(subprocess.check_output([PATH, *ARGS], input=dump(M).encode()).decode())
+# Or
+def flatter_(M):
+    import os
+    import re
+    from subprocess import check_output
+    # compile https://github.com/keeganryan/flatter and put it in $PATH
+    z = "[[" + "]\n[".join(" ".join(map(str, row)) for row in M) + "]]"
+    env = os.environ.copy()
+    env["OMP_NUM_THREADS"] = str(os.cpu_count())
+    ret = subprocess.check_output(["flatter"], input=z.encode(), env=env)
+    return matrix(M.nrows(), M.ncols(), map(int, findall(rb"-?\d+", ret)))
 
 def BabaiCVP(basis, target):
     basis = basis.LLL()
